d47097d541d5cc201fbd755ceb0f223b270d7071
Raphael Marques <jose.raphael.marques@gmail.com>
2019-08-24 08:13:59 -0300

    aula 1 (scripts Andar e Pular)

---
+++ Assets/Aulas/Aula1/Scripts/Andar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Andar : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //-1 esquerda
+        //0 parado
+        //1 direita
+        float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+
+        Rigidbody2D r = GetComponent<Rigidbody2D>();
+
+        Vector2 vel = r.velocity;
+        vel.x = h * speed;
+        r.velocity = vel;
+        
+    }
+}
---
+++ Assets/Aulas/Aula1/Scripts/Pular.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pular : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Rigidbody2D r = GetComponent<Rigidbody2D>();
+            r.velocity = new Vector2(0, speed);
+        }
+    }
+}
